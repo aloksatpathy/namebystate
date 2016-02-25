c57941be46e36b5a700ace31200ed340f5fe94b8
Alok Satpathy <aloks1990@gmail.com>
2016-02-25 13:18:46 -0600

    Started learning pandas

---
+++ learnpython/learnpandas.py
@@ -0,0 +1,5 @@
+import pandas as pd
+#Import Library Pandas
+df = pd.read_csv("C:/Users/Alok Satpathy/Desktop/state/namebystate/namebystate/learnpython/test.txt")  #I am working in Windows environment
+#Reading the dataset in a dataframe using Pandas
+print(df.head(3))  #Print first three observations
