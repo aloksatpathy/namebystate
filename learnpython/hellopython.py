9aa74393558916452545d39ba6030dea5946e688
Alok Satpathy <aloks1990@gmail.com>
2016-02-24 20:18:54 -0600

    Learnt lists

--- learnpython/hellopython.py
+++ learnpython/hellopython.py
@@ -45,3 +45,42 @@ print("I don't like ", end="")
 print("newlines")
 
 
+###List operations
+grocery_list = ['Juice', 'Tomatoes', 'Potatoes',
+                'Bananas', 'Apples']
+print('First item', grocery_list[0])
+
+grocery_list[0]="Orange Juice"
+print('First item', grocery_list[0])
+print(grocery_list[1:3])
+
+other_events = ['Wash Car', 'Cash Check', 'Have breakfast']
+
+to_do_list = [grocery_list, other_events]
+print(to_do_list)
+print((to_do_list[1][1]))
+
+to_do_list.append("Onions")
+print(to_do_list)
+
+grocery_list.insert(3,"Strawberries")
+print(to_do_list)
+
+to_do_list.remove("Onions")
+print("removed Onions",to_do_list)
+
+grocery_list.sort()
+print("sorted",grocery_list)
+
+grocery_list.reverse()
+print("reverse sorted",grocery_list)
+
+del grocery_list[4]
+print(grocery_list)
+
+to_do_list2=grocery_list+other_events
+print(to_do_list2)
+
+print(len(to_do_list2))
+print(max(to_do_list2))
+print(min(to_do_list2))
