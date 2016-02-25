bc3df21216335c5a8fb35085cd57735c610a5ba5
Alok Satpathy <aloks1990@gmail.com>
2016-02-24 20:40:44 -0600

    Learnt dictionaries

--- learnpython/hellopython.py
+++ learnpython/hellopython.py
@@ -97,3 +97,18 @@ print(len(new_tuple),max(new_tuple),min(new_tuple),len(pi_tuple))
 
 
 
+###dictionaries
+super_villains = {"Fiddler": "Isaac Bowin",
+                  "Captain Cold": "Leonard Snart",
+                  "Weather Wizard": "Mark Mardon",
+                  "Mirror Master": "Sam Scudder",
+                  "Pied Piper": "Thomas Peterson"}
+print("Captain Cold =",super_villains["Captain Cold"])
+
+del super_villains["Fiddler"]
+super_villains["Pied Piper"]="Hartley Rathaway"
+print(len(super_villains))
+print(super_villains.get("Pied Piper"))
+
+print(super_villains.keys())
+print(super_villains.values())
