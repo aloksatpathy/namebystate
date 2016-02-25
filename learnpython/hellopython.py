b7e4afedc63c6e1e7f0015116f0bcc626aad5bfe
Alok Satpathy <aloks1990@gmail.com>
2016-02-24 19:56:40 -0600

    Learnt string operations

--- learnpython/hellopython.py
+++ learnpython/hellopython.py
@@ -20,6 +20,7 @@ datatypes : Numbers Strings Lists Tuples Dictionaries
 // - perform division and discard remainder
 '''
 
+###Arithmetic operations
 print("5 + 2 = ",5+2)
 print("5 - 2 = ",5-2)
 print("5 * 2 = ",5*2)
@@ -27,3 +28,20 @@ print("5 / 2 = ",5/2)
 print("5 % 2 = ",5%2)
 print("5 ** 2 = ",5**2)
 print("5 // 2 = ",5//2)
+
+
+###String operations
+quote = "\"Always remember you are unique"
+multi_line_quote = ''' just
+like everyone else\"'''
+
+new_string = quote + multi_line_quote
+
+print("%s %s %s" % ("I like the quote", quote, multi_line_quote))
+
+print ("\n"*5)
+
+print("I don't like ", end="")
+print("newlines")
+
+
