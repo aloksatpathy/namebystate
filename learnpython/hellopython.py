19ad30b5d9aa78faf2e2fc0d0ec1f326717434e5
Alok Satpathy <aloks1990@gmail.com>
2016-02-24 19:44:29 -0600

    learnt algorithmic operations

--- learnpython/hellopython.py
+++ learnpython/hellopython.py
@@ -10,3 +10,20 @@ print ("Hello World")
 Multiline
 comments
 '''
+
+'''
+datatypes : Numbers Strings Lists Tuples Dictionaries
+7 algorithmic operators:
++ - * /
+% - remainder of a division
+** - exponential operator
+// - perform division and discard remainder
+'''
+
+print("5 + 2 = ",5+2)
+print("5 - 2 = ",5-2)
+print("5 * 2 = ",5*2)
+print("5 / 2 = ",5/2)
+print("5 % 2 = ",5%2)
+print("5 ** 2 = ",5**2)
+print("5 // 2 = ",5//2)
