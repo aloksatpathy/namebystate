25f8a61ef5246353bccaaa1aefd298dac759dbe7
Alok Satpathy <aloks1990@gmail.com>
2016-02-25 02:46:17 -0600

    Learnt Class, Constructor, Inheritance, Overriding functions, Method overloading, Polymorphism

--- learnpython/hellopython.py
+++ learnpython/hellopython.py
@@ -215,6 +215,8 @@ split_list=long_string.split(" ")
 print(split_list)
 
 
+
+###File IO operations
 test_file = open("test.txt", "wb")
 print(test_file.mode)  ###mode in which file is opening
 print(test_file.name)  ###file name
@@ -224,3 +226,103 @@ test_file.close()
 test_file = open("test.txt", "r+")
 text_in_file = test_file.read()
 print(text_in_file)
+
+
+###Class, Constructor, Inheritance, Overriding functions, Method overloading, Polymorphism
+class Animal :
+    __name = None
+    __height = 0
+    __weight = 0
+    __sound = 0
+
+    def __init__(self, name, height, weight, sound):
+        self.__name = name
+        self.__height = height
+        self.__weight = weight
+        self.__sound = sound
+
+    def set_name(self,name):
+        set.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def set_height(self,height):
+        set.__height = height
+
+    def get_height(self):
+        return self.__height
+
+    def set_weight(self,weight):
+        set.__weight = weight
+
+    def get_weight(self):
+        return self.__weight
+
+    def set_sound(self,sound):
+        set.__sound = sound
+
+    def get_sound(self):
+        return self.__sound
+
+    def get_type(self):
+        print("Animal")
+
+    def toString(self):
+        return "{} is {} cm tall and {} kilograms and says {}".format(self.__name,
+                                                                     self.__height,
+                                                                     self.__weight,
+                                                                     self.__sound)
+
+cat = Animal("Whiskers", 33, 10, "Meow")
+print(cat.toString())
+
+class Dog(Animal):
+    __owner = ""
+
+    def __init__(self, name, height, weight, sound, owner):
+        self.__owner = owner
+        ###super(Dog,self).__init__(name, height, weight, sound)   didn't work have to further analyze
+        self.__name = name
+        self.__height = height
+        self.__weight = weight
+        self.__sound = sound
+
+    def set_owner(self,owner):
+        self.__owner = owner
+
+    def get_owner(self):
+        return self.__owner
+
+    def get_type(self):
+        print("Dog")
+
+    def toString(self):
+        return "{} is {} cm tall and {} kilograms and says {} His owner is {}".format(self.__name,
+                                                                                      self.__height,
+                                                                                      self.__weight,
+                                                                                      self.__sound,
+                                                                                      self.__owner)
+
+    def multiple_sounds(self,how_many=None):
+        if how_many is None:
+            print(self.get_sound())
+        else:
+            print(self.get_sound()*how_many)
+
+spot = Dog("Spot", 53, 27, "Ruff", "Alok")
+
+print(spot.toString())
+
+class AnimalTesting:
+    def get_type(self,animal):
+        animal.get_type()
+
+test_animals = AnimalTesting()
+
+test_animals.get_type(cat)
+test_animals.get_type(spot)
+
+
+spot.multiple_sounds(4) ###not working needs to be investigated
+spot.multiple_sounds()
