a7712b312e5b007600a0636b058c89b2e1724ddf
Alok Satpathy <aloks1990@gmail.com>
2016-02-26 00:40:11 -0600

    Learnt pickling both pandas and normal

--- learnpython/pickling.py
+++ learnpython/pickling.py
@@ -4,21 +4,39 @@ import pickle
 
 ###pandas pickling
 api_key = open("quandlapikey.txt","r").read()
-fiddy_states = pd.read_html("https://simple.wikipedia.org/wiki/List_of_U.S._states")
 
-main_df = pd.DataFrame()
+def state_list():
+    fiddy_states = pd.read_html("https://simple.wikipedia.org/wiki/List_of_U.S._states")
+    return fiddy_states[0][0][1:]
 
-print("main_df",main_df)
+def grab_initial_state_data():
+    states=state_list()
+    main_df = pd.DataFrame()
 
-for abbv in fiddy_states[0][0][1:]:    #[1:] is to ignore the column header that is Abbreviation
-    query = "FMAC/HPI_"+str(abbv)
-    df = Quandl.get(query, authtoken=api_key)
-    df.columns=[str(abbv)]
+    for abbv in states:    #[1:] is to ignore the column header that is Abbreviation
+        query = "FMAC/HPI_"+str(abbv)
+        df = Quandl.get(query, authtoken=api_key)
+        df.columns=[str(abbv)]              #added this to ensure each column has a unique name
 
-    if main_df.empty:
-        main_df = df
-    else:
-        main_df = main_df.join(df)
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df)
+
+    print(main_df.head())
+
+    pickle_out = open('fiddy_states.pickle','wb')
+    pickle.dump(main_df,pickle_out)
+    pickle_out.close()
+
+# grab_initial_state_data()
+
+pickle_in = open("fiddy_states.pickle","rb")
+HPI_data = pickle.load(pickle_in)
+print(HPI_data)
 
 
-print(main_df.head())
+HPI_data.to_pickle("pickle.pickle")
+HPI_data2 = pd.read_pickle("pickle.pickle")
+print(HPI_data2)
+
